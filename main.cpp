90f9c8bff61aeec020841d0da3c8f7ea871d6614
avgCBMenjoyer <pmw6287@nyu.edu>
2024-04-02 16:28:48 +0400

    finished ackermann map

--- main.cpp
+++ main.cpp
@@ -1,4 +1,36 @@
 #include "tree.h"
 
+using namespace std;
 
+
+node* pacman(int A, int B){
+    int x=A, y=B;
+    node* root = new node(A, B);
+    node* current, next;
+    current = root;
+    while(x!=0 && y!=0){
+        if(x==0){
+            current->left = new node(y+1);
+            y--;
+        }
+        else if(y==0){
+            current->right = new node(x-1,1);
+            current = current->right;
+        }
+        else{
+            current->left = new node(x-1);
+            current->right = new node(x,(y-1));
+            current = current->right;
+        }
+    }
+    return root;
+
+
+}
+
+//crappy recursion traversal function cause im lazy
+void scalePacman(node* root){
+    node* current = root;
+    
+}
 
--- tree.h
+++ tree.h
@@ -10,7 +10,7 @@ struct node{
     node* right;
 
     node(){
-        ID=++counter;
+        ID=-1;
         m =-1;
         n = -1;
         data = -1;
@@ -19,8 +19,35 @@ struct node{
     }
 
     node(int newM, int newN){
-        ID = ++counter;
+        ID=-1;
         m = newM;
         n = newN;
+        data = -1;
+        left = nullptr;
+        right = nullptr;
     }
+
+    node(int newData){ //leaf node
+        data = newData;
+        ID = ++counter; //counter only for leaves
+        m = -1;
+        n = -1;
+        left = nullptr;
+        right = nullptr;
+    }
+
+    void setLeft(node* west){
+        left = west;
+    }
+    void setRight(node* east){
+        right = east;
+    }
+    void makeLeaf(){
+        data = n+1; //ackerman y+1
+        left = nullptr;
+        right = nullptr;
+        m =-1;
+        n=-1;
+    }
+    
 };
