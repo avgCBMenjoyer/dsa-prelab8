3333d94b9b044ad0b16c8fb998e98c7d3cf7e11f
avgCBMenjoyer <pmw6287@nyu.edu>
2024-04-17 09:59:03 +0400

    added edging

--- 9prelab/tree.cpp
+++ 9prelab/tree.cpp
@@ -44,26 +44,64 @@ Tree * eval_expr();
 
 Tree * eval_prim()
 {
-    // FIXME
-    return nullptr;
+    if ( peek == "(" )
+    {
+        tok();
+        return eval_expr();
+    }
+
+    return new Tree(tok());
 }
 
 Tree * eval_powr()
 {
-    // FIXME
-    return nullptr;
+    auto result = eval_prim();
+
+    for (;;)
+    {
+        if ( peek == "^" )
+        {
+            auto op = tok();
+            auto b = eval_prim();
+            result = new Tree(op, result, b);
+        }
+        else break;
+    }
+    return result;
 }
 
 Tree * eval_term()
 {
-    // FIXME
-    return nullptr;
+    auto result = eval_powr();
+    for (;;)
+    {
+        if ( peek == "*" || peek == "/" || peek == "%" )
+        {
+            auto op = tok();
+            auto b = eval_powr();
+            result = new Tree(op, result, b);
+        }
+        else break;
+    }
+    return result;
 }
 
 Tree * eval_expr()
 {
-    // FIXME
-    return nullptr;
+    Tree * result = eval_term();
+    for (;;)
+    {
+        if ( peek == "+" || peek == "-" )
+        {
+            auto op = tok();
+            auto b = eval_term();
+            result = new Tree(op, result, b);
+        }
+        else if (peek.empty()) break;
+        else if (peek == ")") { tok(); break; }
+        else throw "bad operator [" + peek + "]";
+    }
+    return result;
 }
 
 int main()
@@ -82,14 +120,15 @@ try
         tok(); // load peek token
 
         Tree * tree = eval_expr();
-
+        /*
         cout << "```mermaid\ngraph TD\n"
              << "A(\"" << line << "\")\n"
              << "B(\"" << tree->expr() << "\")\n"
              << "A --> B\n"
              << "style A fill:#ded\n"
-             << "style B fill:#dde\n"
-             << tree->edges() << "```\n---\n";
+             << "style B fill:#dde\n" */
+            cout << tree->edges() << "```\n---\n";
+            cout << tree->expr() << "\n";
 
         delete tree;
     }
@@ -105,14 +144,65 @@ string Tree::str() const
 }
 
 string Tree::edges() const
-{
-    // FIXME
+{   //Base case for leaves
+    if(right==nullptr && left==nullptr){
+        return str();
+    }
+
+    cout << str() << " --> "; 
+    if(left!=nullptr){
+        cout << left->str();
+    }
+    cout << "\n";
+    cout << str() << " --> "; 
+    if(right!=nullptr){
+        cout << right->str();
+    }
+    cout << "\n";
+    if(left!=nullptr && !(left->op.empty())){
+        cout << left->edges();
+    }
+    
+    if(right!=nullptr && !(right->op.empty())){
+        cout << right->edges();
+    }
+    
+    
     return "";
+
 }
 
 
 string Tree::expr() const
 {
-    // FIXME
-    return "";
+    
+    
+    if(left->op.empty()){
+        return left->val + " " + op + " " + right->val;        
+    }
+
+    string result;
+    if(op == "^"){
+        if(left->op != "^"){
+            cout << " ( " + left->expr() + " ) ";
+        }
+        if(right->op != "^"){
+            cout << " ( " + right->expr() + " ) ";
+        }
+
+
+    }
+    else if(op == "*" || op == "/" || op == "%"){
+        if(left->op == "+" || left->op == "-"){
+            cout << " ( " + left->expr() + " ) ";
+        }
+        if(right->op == "+" || right->op == "-"){
+            cout << " ( " + right->expr() + " ) ";
+        }
+
+    }
+        
+    
+    return result;
+
 }
